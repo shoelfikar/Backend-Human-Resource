9e9ef759e6a545e41bd6331990d237228f22e717
sulfikardi25 <sulfikardi25@gmail.com>
2020-06-21 23:29:12 +0700

    gitignore add

--- src/middleware/sendMail.js
+++ src/middleware/sendMail.js
@@ -8,12 +8,6 @@ const sendMail = (mailOptions) => {
       pass : process.env.EMAIL_PASS
     }
   })
-  // const mailOptions = {
-  //   from: process.env.EMAIL,
-  //   to: user.email,
-  //   subject: 'Registrasi Akun',
-  //   html: `<p>Hi ${user.nama_lengkap}, Silahkan Klik tautan dibawah ini untuk konfirmasi alamat email anda</p>`
-  // }
   transporter.sendMail(mailOptions, (error, info)=> {
     if(error){
       console.log(error)
